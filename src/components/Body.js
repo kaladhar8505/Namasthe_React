ef75d82dbf3ddb936e0fac4934a8e0dd686aba97
kaladhar8505 <kaladharkhamithkar@gmail.com>
2023-07-02 03:15:54 +0530

    Exploring the world Ep6: usestate started

--- src/components/Body.js
+++ src/components/Body.js
@@ -23,11 +23,12 @@ const Body = () => {
     }
 
 
-    if(listOfRestaurants.length === 0){
-        return<Shimmer />
-    }
+    // Conditional Rendering
+    // if(listOfRestaurants.length === 0){
+    //     return<Shimmer />
+    // }
 
-    return (
+    return listOfRestaurants.length === 0 ? <Shimmer /> : (
         <div className="body">
             <div className="filter">
                 <button 
