34e2d09facb6522a72f6b6b945b279400bc3815b
K Kaladhar <kaladharkhamithkar@gmail.com>
2023-08-29 12:12:20 +0530

    Ep.08 End of Part.01

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,13 @@
+import User from "./User";
+import UserClass from "./UserClass";
 const About = () => {
     return (
         <div>
             <h1>About</h1>
             <h2>This is Namaste React Web Series</h2>
+            <User name = {"Kaladhar Khamithkar (function) "}/>
+
+            <UserClass name={"Kaladhar Khamithkar (class)"} location={"Bangalore"} contact={"@kaladhark"}/>
         </div>
     );
 };
---
+++ src/components/User.js
@@ -0,0 +1,10 @@
+const User = ({name}) => {
+    return (<div className="user-card">
+        <h2>Name: {name}</h2>
+        <h3>Location: Bangalore</h3>
+        <h4>Contact: @kaladhar</h4>
+    </div>
+    );
+};
+
+export default User;
