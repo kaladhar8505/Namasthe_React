109385b987d89822c0dadf0518ddf716fd54e913
K Kaladhar <kaladharkhamithkar@gmail.com>
2023-08-29 12:22:15 +0530

    starting of Ep.08 Part 2

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,12 +4,15 @@ class UserClass extends React.Component{
 
     constructor(props){
         super(props);
-        console.log(props);
+        this.state= {
+            count: 0,
+        };
     }
     render () {
 
         const {name, location, contact} = this.props;
         return (<div className="user-card">
+            <h1>Count: {this.state.count}</h1>
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h4>Contact: {contact}</h4>
